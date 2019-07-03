54891c52759c832bffa50fbbc9e871378bab0d27
jyunki <jyunki9780@gmail.com>
2019-07-03 12:18:11 +0000

    rails06kadai

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,31 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+  
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    # @topic = current_user.topics.new(topic_params)
+    @topic = Topic.new(topic_params)
+    # 
+    # @topic = Topic.new
+    # @topic.image = topic_params[:image]
+    # @topic.description = topic_params[:description]
+    @topic.user_id = current_user.id
+
+    if @topic.save
+      redirect_to topics_path, success: '投稿に成功しました'
+    else
+      flash.now[:danger] = "投稿に失敗しました"
+      render :new
+    end
+  end
+
+  private
+  def topic_params
+    params.require(:topic).permit(:image, :description)
+  end
+end
