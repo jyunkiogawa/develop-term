98f3c9c2c2be177a1d4884c9a8fb7709a3c7ff63
jyunki <jyunki9780@gmail.com>
2019-07-19 05:02:05 +0000

    kadai07

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,14 +6,22 @@ class FavoritesController < ApplicationController
   end
 
   def create
-    favorite = Favorite.new
-    favorite.user_id = current_user.id
-    favorite.topic_id = params[:topic_id]
-
+    favorite = Favorite.new(user_id: current_user.id, topic_id: params[:topic_id])
+    # favorite.user_id = current_user.id
+    # favorite.topic_id = params[:topic_id]
+# binding.pry
     if favorite.save
       redirect_to topics_path, success: 'お気に入りに登録しました'
     else
       redirect_to topics_path, danger: 'お気に入りに登録に失敗しました'
     end
   end
+  
+  def destroy
+    favorite = Favorite.find_by(user_id: current_user.id, topic_id: params[:topic_id])
+    
+    if favorite.destroy
+      redirect_to topics_path, success: 'お気に入りを削除しました'
+    end
+  end  
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,4 @@
 class Topic < ApplicationRecord
-  validates :user_id, presence: true
   validates :description, presence: true
   validates :image, presence: true
   
@@ -8,7 +7,20 @@ class Topic < ApplicationRecord
   mount_uploader :image, ImageUploader
   
   has_many :favorites
+
+  # def  favorites 
+  #   Favorite.where(topic_id: id)
+    
+  # end
   has_many :favorite_users, through: :favorites, source: 'user'
+  def favorite_users
+    users = []
+    favorites = Favorite.where(topic_id: id)
+    favorites.each do |favorite|
+      users << User.find_by(id: favorite.user_id)
+    end
+    return users
+  end
   
   has_many :comments
 end
